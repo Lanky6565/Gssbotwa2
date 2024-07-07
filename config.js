97fbb8900822486b805e5accf59a550cf76d9ee5
Lanky6565 <139048726+Lanky6565@users.noreply.github.com>
2024-07-07 15:35:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
 global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
+global.owner = ['2349157321797'];
 global.premium = ['917050906659'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
 global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
@@ -18,13 +18,13 @@ global.typemenu = "v1"
 global.menuType = "2"
 global.autoswview = true
 global.autoread = false
-global.available = false
+global.available = true
 global.autoTyping = false
 global.autoRecord = false
 global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
-global.welcome = true
+global.welcome = false
 global.autoBlock = true
  
 /*REPLY MESSAGE*/
